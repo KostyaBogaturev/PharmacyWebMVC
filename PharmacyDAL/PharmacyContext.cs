a69c0873ff3b27bd256088f76fd1629125c801c3
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-08-17 13:44:31 +0300

    Add  context to connection to db and productRepository.cs

---
+++ PharmacyDAL/PharmacyContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PharmacyDAL.Entities;
+
+namespace PharmacyDAL
+{
+    public class PharmacyContext : DbContext
+    {
+        public DbSet<Pharmacy> Pharmacies { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        static PharmacyContext()
+        {
+
+        }
+
+        public PharmacyContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=PharmacyDB;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ PharmacyDAL/Repositories/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PharmacyDAL.Contracts;
+using PharmacyDAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PharmacyDAL.Repositories
+{
+    public class ProductRepository : IRepository<Product>
+    {
+        private PharmacyContext _ctx;
+
+        public ProductRepository(PharmacyContext context)
+        {
+            _ctx = context;
+        }
+
+        public void Create(Product item) => _ctx.Products.Add(item);
+
+        public void Delete(Guid id)
+        {
+            Product product = _ctx.Products.Find(id);
+            if (product != null)
+                _ctx.Products.Remove(product);
+        }
+
+        public Product Get(Guid id) => _ctx.Products.Find(id);
+
+
+        public IEnumerable<Product> GetAll() => _ctx.Products;
+
+
+        public void Update(Product item) => _ctx.Entry(item).State = EntityState.Modified;
+
+    }
+}
