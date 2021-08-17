99bd17fb75e85a1eec652c7734d2aa6998583ea5
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-08-18 02:03:51 +0300

    Added new classes to DTO

---
+++ PharmacyBLL/DTO/RoleDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PharmacyBLL.DTO
+{
+    public class RoleDTO
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IEnumerable<UserDTO> Users { get; set; }
+    }
+}
---
+++ PharmacyBLL/DTO/UserDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PharmacyBLL.DTO
+{
+    public class UserDTO
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public Guid? RoleId { get; set; }
+
+        public RoleDTO Role { get; set; }
+    }
+}
