5e64c65c5f41e8c362ea97036f2660c4af0991b6
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-08-18 01:48:19 +0300

    Created output of products list add page for creating new product

---
+++ PharmacyWeb/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PharmacyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<ProductViewModel> products;
+
+        public IActionResult Index()
+        {
+
+            CreateData();
+            return View(products);
+        }
+
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductViewModel product)
+        {
+            if (product != null)
+            {
+                product.Id = Guid.NewGuid();
+                products.Add(product);
+                return RedirectToAction("Index");
+            }
+            return View("Index");
+        }
+
+            private static void CreateData()
+        {
+            if (products == null)
+            {
+                products = new List<ProductViewModel>()
+                {
+                    new ProductViewModel()
+                    {
+                        Id=Guid.NewGuid(),
+                        Name= "Name 1"
+                    },
+                    new ProductViewModel()
+                    {
+                        Id=Guid.NewGuid(),
+                        Name= "Name 2"
+                    }
+                };
+
+
+            }
+        }
+    }
+}
---
+++ PharmacyWeb/Models/ProductViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyWeb.Models
+{
+    public class ProductViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Firm { get; set; }
+
+        public double Cost { get; set; }
+
+        public int Count { get; set; }
+
+        public string Image { get; set; }
+    }
+}
