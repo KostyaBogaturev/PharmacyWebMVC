593d1104cc345211ad7687876b3052e4085258fa
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-08-18 15:10:53 +0300

    Added Iaministrator interface

---
+++ PharmacyBLL/Contracts/IAdministrator.cs
@@ -0,0 +1,22 @@
+﻿using PharmacyBLL.DTO;
+using System;
+using System.Collections.Generic;
+
+namespace PharmacyBLL.Contracts
+{
+    interface IAdministrator
+    {
+        IEnumerable<ProductDTO> GetProducts();
+        ProductDTO GetProduct(Guid id);
+        void Updata(ProductDTO product);
+        void Create(ProductDTO product);
+        void Delete(ProductDTO product);
+
+        IEnumerable<PharmacyDTO> GetPharmacies();
+        PharmacyDTO GetPharmacy(Guid id);
+        void Updata(PharmacyDTO pharmacy);
+        void Create(PharmacyDTO pharmacy);
+        void Delete(PharmacyDTO pharmacy);
+
+    }
+}
