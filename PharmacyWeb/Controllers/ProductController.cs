c83f6a339b3199cad6debd3f2b787b3dac58b70f
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-08-20 14:26:01 +0300

    Created ProductController and empty view for it

---
+++ PharmacyWeb/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PharmacyBLL.DTO;
+using PharmacyBLL.Services;
+using PharmacyWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PharmacyWeb.Controllers
+{
+    public class ProductController:Controller
+    {
+        private AdministratorService administratorService;
+        private IMapper mapper = AutoMapperConfig.GetMapper();
+        public IActionResult Index()
+        {
+            administratorService = new AdministratorService();
+            administratorService.GetProducts();
+            IEnumerable<ProductDTO> productsDTO = administratorService.GetProducts();
+            var products = mapper.Map<IEnumerable<ProductDTO>, IEnumerable<ProductViewModel>>(productsDTO);
+            return View(products);
+        }
+    }
+}
