05cd86f122317875833a4c658db158f53ac2d196
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-08-22 14:00:07 +0300

    Add method for creating product

--- PharmacyWeb/Controllers/ProductController.cs
+++ PharmacyWeb/Controllers/ProductController.cs
@@ -14,12 +14,32 @@ namespace PharmacyWeb.Controllers
     {
         private AdministratorService administratorService;
         private IMapper mapper = AutoMapperConfig.GetMapper();
+
         public IActionResult Index()
         {
             administratorService = new AdministratorService();
             IEnumerable<ProductDTO> productsDTO = administratorService.GetProducts();
             var products = mapper.Map<List<ProductViewModel>>(productsDTO);
             return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult CreateProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(ProductViewModel product)
+        {
+            if(product != null)
+            {
+                administratorService = new AdministratorService();
+                var productDTO = mapper.Map<ProductDTO>(product);
+                administratorService.Create(productDTO);
+                return Redirect("Index");
+            }
+            return View();
         }
     }
 }
