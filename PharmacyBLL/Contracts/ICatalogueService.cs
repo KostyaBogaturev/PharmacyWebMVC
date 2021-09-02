893c8066594a9d7b8ee219a64b9e7a4b0fed62c5
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-09-02 23:20:48 +0300

    Created interface ICatalogueService

---
+++ PharmacyBLL/Contracts/ICatalogueService.cs
@@ -0,0 +1,15 @@
+﻿using PharmacyBLL.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PharmacyBLL.Contracts
+{
+    public interface ICatalogueService
+    {
+        public Task<IEnumerable<ProductDTO>> GetAllProductsAsync();
+        public IEnumerable<MedicineDTO> GetMedicines();
+        public IEnumerable<BeautyDTO> GetBeauties();
+        public IEnumerable<ProductForChildDTO> GetProductsForChildren();
+        public IEnumerable<ProductDTO> GetDiscountProducts();
+    }
+}
---
+++ PharmacyBLL/Services/CatalogueService.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using PharmacyBLL.Contracts;
+using PharmacyBLL.DTO;
+using PharmacyDAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmacyBLL.Services
+{
+    class CatalogueService : ICatalogueService
+    {
+        private IMapper mapper;
+        private UnitOfWork dataBase;
+
+        public CatalogueService()
+        {
+            mapper = AutoMapperConfig.GetMapper();
+            dataBase = new UnitOfWork();
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetAllProductsAsync()
+        {
+            var productsDAL = await dataBase.Products.GetAllAsync();
+            var products = mapper.Map<List<ProductDTO>>(productsDAL);
+            return products;
+        }
+
+        public IEnumerable<BeautyDTO> GetBeauties()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<MedicineDTO> GetMedicines()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProductForChildDTO> GetProductsForChildren()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ProductDTO> GetDiscountProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
