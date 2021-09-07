78ce48e94402a41babfbfcfe7f792fdbb640408b
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-09-07 18:52:46 +0300

    Changet application context in PharmacyWeb. Added method EnshureCreated into ctor of class ApplicationContext

--- PharmacyWeb/Data/ApplicationDbContext.cs
+++ PharmacyWeb/Data/ApplicationDbContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace PharmacyWeb.Data
@@ -11,6 +8,7 @@ namespace PharmacyWeb.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            Database.EnsureCreated();
         }
     }
 }
