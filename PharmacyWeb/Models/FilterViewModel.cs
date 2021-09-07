c0b358d14484eab2def6f8c976736ef058c32ce9
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-09-07 12:10:14 +0300

    Created FilterViewModel

---
+++ PharmacyWeb/Models/FilterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace PharmacyWeb.Models
+{
+    public class FilterViewModel
+    {
+        public SelectList Firms { get; set; }
+
+        public string SelectedFirm { get; set; }
+
+        public FilterViewModel(List<string> companies, string selectedFirm)
+        {
+            companies.Insert( 0,"All");
+            Firms = new SelectList(companies,selectedFirm);
+            SelectedFirm = selectedFirm;
+        }
+    }
+}
