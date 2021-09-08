d0507e28fa0f2eb15b2e83f7fc44f464ab57be54
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2021-09-08 15:01:03 +0300

    Created class which will make filtration of products

---
+++ PharmacyBLL/Services/FiltrationService.cs
@@ -0,0 +1,46 @@
+﻿using PharmacyBLL.DTO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PharmacyBLL.Services
+{
+    public class FiltrationService
+    {
+        public IEnumerable<ProductDTO> FilterByFirm(IEnumerable<ProductDTO> products, string firmName)
+        {
+            if (products == null)
+                return null;
+            var result = products.Where(p => p.Firm == firmName);
+            return result;
+        }
+
+        public IEnumerable<ProductDTO> FilterByAvailability(IEnumerable<ProductDTO> products, bool IsStockOnly)
+        {
+            if (products == null)
+                return null;
+
+            if (IsStockOnly)
+                products = products.Where(p => p.Count > 0);
+
+            return products;
+        }
+
+
+        public IEnumerable<ProductDTO> FilterByType(IEnumerable<ProductDTO> products, string typeName)
+        {
+            if (products == null)
+                return null;
+            var result = products.Where(p => p.Subtype.Type.Name==typeName);
+            return result;
+        }
+
+        public IEnumerable<ProductDTO> FilterBySubtype(IEnumerable<ProductDTO> products, string subtypeName)
+        {
+            if (products == null)
+                return null;
+            var result = products.Where(p => p.Subtype.Name == subtypeName);
+            return result;
+        }
+
+    }
+}
